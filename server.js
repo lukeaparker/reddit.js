d15fefafdc66d2288ba6d77186b010b79e116ec7
Luke Parker <lukeaparker@gmail.com>
2020-04-16 12:45:21 -0700

    added num basses, recursion, search

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+require('./controllers/posts.js')(app);
 const express = require('express');
 const mongoose = require('mongoose')
-
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+// Set db
+require('./data/reddit-db');
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Add after body parser initialization!
+app.use(expressValidator());
 // App Setup
 const app = express();
 
@@ -21,21 +30,25 @@ app.get('/', (req, res) => {
     res.render('home')
   })
 
-  app.get('/posts', (req, res) => {
-    const all_posts = Post.find()
+app.get('/posts', (req, res) => {
+const all_posts = Post.find()
     res.render('posts', {all_posts})
-
-
-
-    
 })
 
-  app.get('/posts/new', (req, res) => {
+app.get('/posts/new', (req, res) => {
     res.render('posts-new')
-  })
+})
+
+Post.find({})
+.then(posts => {
+    res.render("posts-index", { posts });
+})
+.catch(err => {
+    console.log(err.message);
+});
 
 
-  // Start Server
+// Start Server
 app.listen(3000, () => {
     console.log('Reddit Search listening on port localhost:3000!');
-  });
+});
